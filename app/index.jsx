d2aab5322300660ad97b92837d4f5d469a840ca5
feyhide <fahadsheikhessential@gmail.com>
2024-09-14 17:29:14 +0500

    added fonts and assets

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,13 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet,Text,View } from "react-native";
+import { Text,View } from "react-native";
 
 export default function App(){
     return(
-        <View style={styles.container}>
-            <Text>Aora!</Text>
+        <View className="flex-1 items-center justify-center bg-white" >
+            <Text className='text-5xl font-pblack'>Aora!</Text>
             <StatusBar style="auto"/>
+            <Link href="/profile">go to profile</Link>
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        backgroundColor:"#fff",
-        alignItems:"center",
-        justifyContent:"center"
-    }
-})
