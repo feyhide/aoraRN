43d61a37da0157195feb242ce0e81f36e80fef67
feyhide <fahadsheikhessential@gmail.com>
2024-09-14 17:46:48 +0500

    oops

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-<<<<<<< HEAD
     plugins: ["nativewind/babel"],
-=======
->>>>>>> 76c4aa13260e8473388da35e7cea54d3fabb4cf0
   };
 };
